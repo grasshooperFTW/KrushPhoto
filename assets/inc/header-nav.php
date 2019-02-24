5b9dea748a11c425b5543a7251c566e733696cf4
grasshooperFTW <wyszynskim12d@gmail.com>
2019-02-24 16:53:22 +0100

    adding starting project

---
+++ assets/inc/header-nav.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-default navbar-expand-lg" role="navigation-demo">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-translate">
+                <a class="navbar-brand" href="#0">
+                    <div class="navbar-brand">
+                        <div class="logo-big">
+                            <img src="../assets/img/Czarne logo (1).png" class="img-fluid" style="height: 100%">
+                        </div>
+                    </div>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="sr-only">Nawigacja</span>
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
---
+++ assets/inc/meta.php
@@ -0,0 +1,12 @@
+    
+<!--obowiązkowe metadane-->
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+
+    <meta name="author" content="XYZ co.">
+    <!-- <link href= rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet"> -->
+    
+   <link href="favicon32.png" rel="icon">
+    
